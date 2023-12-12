cbf6033f1f20842edfe048fffadc07da90e21fe7
Erta <ertaxhixho@gmail.com>
2023-12-12 14:39:35 +0100

    remove contextLoads in test

--- src/test/java/com/hendisantika/springbootrestapipostgresql/SpringBootRestApiPostgresqlApplicationTests.java
+++ src/test/java/com/hendisantika/springbootrestapipostgresql/SpringBootRestApiPostgresqlApplicationTests.java
@@ -6,9 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class SpringBootRestApiPostgresqlApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+//    @Test
+//    public void contextLoads() {
+//    }
 
 }
 
